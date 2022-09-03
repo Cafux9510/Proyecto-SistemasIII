39fe17ce1fd6e301457381302be00c16aed8d8c2
franco <francocavs02@gmail.com>
2022-09-03 01:37:52 -0300

    CRUD completo

--- Vespucio/src/componentes/Comprobantes.jsx
+++ Vespucio/src/componentes/Comprobantes.jsx
@@ -8,6 +8,27 @@ import { InputText } from 'primereact/inputtext';
 import {makeStyles} from "@material-ui/core/styles"
 import { amber } from "@material-ui/core/colors";
 import swal from "sweetalert";
+import styled from '@emotion/styled'
+
+const Label = styled.label`
+    flex: 0 0 100px;
+    text-align:center;
+
+`;
+
+const Select = styled.select`
+    display:block;
+    width:100%;
+    padding: 1rem;
+    border: 1px solid #e1e1e1;
+    -webkit-appearance:none;
+`
+
+const Campo= styled.div`
+    display:flex;
+    margin-bottom: 1rem;
+    align-items:center;
+`;
 
 
 
@@ -39,31 +60,39 @@ const Comprobantes = () => {
     const [data,setData]=useState([])
     const[modal,insertarModal]=useState(false)
     const [modalEditar, setModalEditar]= useState(false);
-    const[proveedor,proveedorAgregado]=useState({
-        categoria_proveedor:'',
-        nombre_proveedor:'',
-        cuit_proveedor:'',
-        direccion_proveedor:'',
-        localidad_proveedor:'',
-        telefono_proveedor:'',
-        email_proveedor:'',
+    const[comprobante,setComprobante]=useState({
+        tipo_comprobante:'',
+        proveedor_comprobante:'',
+        tipo_movimiento:'',
+        numero_comprobante:'',
+        fecha_emision:'',
+        total_comprobante:'',
+        link_archivo:''
     })
     //Funciones que tienen datos desde una api
     const funcion = async()=>{
         try {
-           const result= await supabase.from('proveedores')
-           .select()
-           .eq("isHabilitado_proveedor",true)
+           const result= await supabase.from('comprobantes')
+           .select(`
+           *,
+           tipoComprobante(
+             nombre_tipo
+           ),
+           proveedores(
+            nombre_proveedor
+           )
+         `)
+           .eq("isHabilitado_comprobante",true)
            setData(result.data)
         } catch (error) {
             console.log(error)
         }
     }
-    const update = async(id_proveedor)=>{
+    const update = async(id_comprobante)=>{
       try {
-        const result= await supabase.from("proveedores")
-        .update({isHabilitado_proveedor:false})
-        .eq("id_proveedor",id_proveedor)
+        const result= await supabase.from("comprobantes")
+        .update({isHabilitado_comprobante:false})
+        .eq("id_comprobante",id_comprobante)
 
 
        
@@ -72,17 +101,30 @@ const Comprobantes = () => {
       }
     }
 
-    const{categoria_proveedor,nombre_proveedor,cuit_proveedor,direccion_proveedor,localidad_proveedor,telefono_proveedor,email_proveedor}=proveedor;
+    const{tipo_comprobante,proveedor_comprobante,tipo_movimiento,numero_comprobante,fecha_emision,total_comprobante,link_archivo}=comprobante;
 
-    const update2=async(id_proveedor)=>{
+    const update2=async(id_comprobante)=>{
       try {
-        const result= await supabase.from("proveedores")
-        .update({categoria_proveedor,nombre_proveedor,cuit_proveedor,direccion_proveedor,localidad_proveedor,telefono_proveedor,email_proveedor})
-        .eq("id_proveedor",id_proveedor)
+          const avatarFile = document.getElementById('selectArchivo').files[0];
+          const  foto = await supabase.storage
+          .from('archivos-subidos')
+          .upload('archivos-comprobantes/'+parseInt(avatarFile.lastModified/avatarFile.size), avatarFile, {
+            cacheControl: '3600',
+            upsert: false,
+          })
+
+          const principio_cadena = 'https://nnlzmdwuqwxgdrnutujk.supabase.co/storage/v1/object/public/';
+          const final_cadena = foto.data.Key
+          const link_archivo= principio_cadena + final_cadena
+        
+        
+        const result= await supabase.from("comprobantes")
+        .update({tipo_comprobante,proveedor_comprobante,tipo_movimiento,numero_comprobante,fecha_emision,total_comprobante,link_archivo})
+        .eq("id_comprobante",id_comprobante)
         
         console.log(result)
         abrirCerrarModalEditar();
-        window.location.reload()
+        window.location.reload(); 
        } catch (error) {
         console.log(error)
       }
@@ -90,28 +132,42 @@ const Comprobantes = () => {
 
     const submit = async()=>{
       try {
-        const {error,result}= await supabase.from("proveedores").insert({
-          categoria_proveedor,
-          nombre_proveedor,
-          cuit_proveedor,
-          direccion_proveedor,
-          localidad_proveedor,
-          telefono_proveedor,
-          email_proveedor
+        const avatarFile = document.getElementById('selectArchivo').files[0];
+        const  foto = await supabase.storage
+        .from('archivos-subidos')
+        .upload('archivos-comprobantes/'+parseInt(avatarFile.lastModified/avatarFile.size), avatarFile, {
+          cacheControl: '3600',
+          upsert: false,
+        })
+
+        const principio_cadena = 'https://nnlzmdwuqwxgdrnutujk.supabase.co/storage/v1/object/public/';
+        const final_cadena = foto.data.Key
+        const link_archivo= principio_cadena + final_cadena
+        
+        const {error,result}= await supabase.from("comprobantes").insert({
+          tipo_comprobante,
+          proveedor_comprobante,
+          tipo_movimiento,
+          numero_comprobante,
+          fecha_emision,
+          total_comprobante,
+          link_archivo
         });
 
+        console.log(final_cadena)
+       
         abrirCerrarModalInsertar();
         setData({
           ...data,
           result
         })
-        window.location.reload()
+       window.location.reload() 
       } catch (error) {
         console.log(error)
       }
     }
 
-    const handleEliminar=(id_proveedor)=>{
+    const handleEliminar=(id_comprobante)=>{
       swal({
         title: "Estas seguro de eliminar este registro?",
         text: "Una vez eliminado, no podras recuperar el archivo de vuelta!",
@@ -124,7 +180,7 @@ const Comprobantes = () => {
           swal("Registro eliminado con exito!", {
             icon: "success",
           });
-          update(id_proveedor);
+          update(id_comprobante);
         }
         setTimeout(() => {
           window.location.reload()
@@ -134,12 +190,13 @@ const Comprobantes = () => {
 
     //Configuracion del {/*DEJO LOS PARAMETROS DEL VALUE, SINO ME CRASHEA */}
     const columnas=[ 
-        {title:"Id", field:"id_categoria_proveedor"},
-        {title:"Tipo", field:"nombre_proveedor"},
-        {title:"Movimiento", field:"cuit_proveedor"},
-        {title:"Numero", field:"direccion_proveedor"},
-        {title:"Fecha Emision",field:"localidad_proveedor"},
-        {title:"Valor total",field:"telefono_proveedor"},
+        {title:"Id", field:"id_comprobante"},
+        {title:"Tipo", field:"tipoComprobante.nombre_tipo"},
+        {title:"Proveedor", field:"proveedores.nombre_proveedor"},
+        {title:"Movimiento", field:"tipo_movimiento"},
+        {title:"Numero", field:"numero_comprobante"},
+        {title:"Fecha Emision",field:"fecha_emision"},
+        {title:"Valor total",field:"total_comprobante"},
         
       ]
 
@@ -147,27 +204,100 @@ const Comprobantes = () => {
     //Estilos
     const styles=useStyles();
     const actualizarState = e =>{
-      proveedorAgregado({
-          ...proveedor,
+      setComprobante({
+          ...comprobante,
           [e.target.name]: e.target.value
       })
   }
+
+
+  //Funciones de extraccion de proveedores y tipo comprobante
+  const[tipo,setTipo]=useState({}) 
+  const datos=async()=>{
+    const result = await supabase.from('tipoComprobante').select();
+
+    setTipo(result.data)
+  }
+
+  const[pro,setPro]=useState({}) 
+  const dato=async()=>{
+    const result = await supabase.from('proveedores').select();
+
+    setPro(result.data)
+  }
+
+  const subirImagen = async()=>{
+    try {
+      const avatarFile = document.getElementById('selectArchivo').files[0];
+      const { data, error } = await supabase.storage
+        .from('archivos-subidos')
+        .upload('archivos-comprobantes/'+parseInt(avatarFile.lastModified/avatarFile.size), avatarFile, {
+          cacheControl: '3600',
+          upsert: false,
+        })
+        console.log()
+
+        const principio_cadena = 'https://nnlzmdwuqwxgdrnutujk.supabase.co/storage/v1/object/public/';
+        const final_cadena = data.Key
+        const url_archivo= principio_cadena + final_cadena
+        
+        return url_archivo
+      
+    } catch (error) {
+      
+    }
+  }
+
+
     const bodyInsertar= (
       <div className={styles.modal}>
-        <h3>Agregar Nuevo Comprobante</h3> {/*DEJO LOS PARAMETROS DEL VALUE, SINO ME CRASHEA */}
-        <TextField className={styles.inputMaterial} label="Tipo Comprobante" onChange={actualizarState} name="categoria_proveedor" value={categoria_proveedor}/> 
+        <h3>Agregar Nuevo Comprobante</h3>
+        <Campo>
+        <Label>Tipo Comprobante</Label>
+          <Select
+                    name='tipo_comprobante'
+                    value={tipo_comprobante}
+                    onChange={actualizarState}
+                >
+                    <option value="">--Seleccione--</option>
+                    {Object.values(tipo).map(pr=>(
+                      <option key={pr.id_tipo} value={pr.id_tipo}>{pr.nombre_tipo}</option>
+                    ))}
+                
+                    
+                  
+            </Select>
+        </Campo>
         <br/>
-        <TextField className={styles.inputMaterial} label="Proveedor"  onChange={actualizarState} name="nombre_proveedor" value={nombre_proveedor} />
+        <Campo>
+        <Label>Nombre Proveedor</Label>
+          <Select
+                    name='proveedor_comprobante'
+                    value={proveedor_comprobante}
+                    onChange={actualizarState}
+                >
+                    <option value="">--Seleccione--</option>
+                    {Object.values(pro).map(pr=>(
+                      <option key={pr.id_proveedor} value={pr.id_proveedor}>{pr.nombre_proveedor}</option>
+                    ))}
+            </Select>
+        </Campo>
         <br/>
-        <TextField className={styles.inputMaterial} label="N° Comprobante" onChange={actualizarState} name="cuit_proveedor" value={cuit_proveedor}/>
+        <TextField type="number" className={styles.inputMaterial} label="N° Comprobante" onChange={actualizarState} name="numero_comprobante" value={numero_comprobante}/>
         <br/>
-        <TextField type="date" className={styles.inputMaterial} label=""  onChange={actualizarState} name="direccion_proveedor" value={direccion_proveedor}/>
+        <TextField type="date" className={styles.inputMaterial}   onChange={actualizarState} name="fecha_emision" value={fecha_emision}/>
         <br/>
-        <TextField className={styles.inputMaterial} label="Tipo Movimiento" onChange={actualizarState} name="localidad_proveedor" value={localidad_proveedor} />
+        <Label>Tipo Movimiento</Label>
+        <Select name="tipo_movimiento" value={tipo_movimiento} onChange={actualizarState}>
+          <option value="">--Seleccione--</option>
+          <option value="Ingreso">Ingreso</option>
+          <option value="Egreso">Egreso</option>
+
+        </Select>
         <br/>
-        <TextField type="file" className={styles.inputMaterial} label="Archivo" onChange={actualizarState} name="telefono_proveedor" value={telefono_proveedor} />
+        <input name='input=file' id='selectArchivo' type='file' />
         <br/>
-        <TextField className={styles.inputMaterial} label="Monto Total" onChange={actualizarState} name="monto_Total" value={email_proveedor} />
+        <TextField type="number" className={styles.inputMaterial} label="Monto Total" onChange={actualizarState} name="total_comprobante" value={total_comprobante} />
         <br/><br/>
         <div align="right">
           <Button color='primary' onClick={()=>submit()} >Insertar</Button>
@@ -176,27 +306,64 @@ const Comprobantes = () => {
       </div>
     )
 
-    const{id_proveedor}=proveedor;
+    const{id_comprobante}=comprobante;
 
     const bodyEditar= (
       <div className={styles.modal}>
         <h3>Editar Comprobante</h3>
-        <TextField className={styles.inputMaterial} label="Tipo Comprobante" onChange={actualizarState} name="tipo_Comprobante" value={proveedor&&categoria_proveedor}/>
+        <Campo>
+        <Label>Tipo Comprobante</Label>
+          <Select
+                    name='tipo_comprobante'
+                    value={comprobante&&tipo_comprobante}
+                    onChange={actualizarState}
+                >
+                    <option value="">--Seleccione--</option>
+                    {Object.values(tipo).map(pr=>(
+                      <option key={pr.id_tipo} value={pr.id_tipo}>{pr.nombre_tipo}</option>
+                    ))}
+                
+                    
+                  
+            </Select>
+        </Campo>
         <br/>
-        <TextField className={styles.inputMaterial} label="Proveedor"  onChange={actualizarState} name="nombre_proveedor" value={proveedor&&nombre_proveedor} />
+        <Campo>
+        <Label>Nombre Proveedor</Label>
+          <Select
+                    name='proveedor_comprobante'
+                    value={comprobante&&proveedor_comprobante}
+                    onChange={actualizarState}
+                >
+                    <option value="">--Seleccione--</option>
+                    {Object.values(pro).map(pr=>(
+                      <option key={pr.id_proveedor} value={pr.id_proveedor}>{pr.nombre_proveedor}</option>
+                    ))}
+                
+                    
+                  
+            </Select>
+        </Campo>
         <br/>
-        <TextField className={styles.inputMaterial} label="N° Comprobante" onChange={actualizarState} name="num_Comprobante" value={proveedor&&cuit_proveedor}/>
+        <TextField type="number" className={styles.inputMaterial} label="N° Comprobante" onChange={actualizarState} name="numero_comprobante" value={comprobante&&numero_comprobante}/>
         <br/>
-        <TextField type="date" className={styles.inputMaterial} label="" onChange={actualizarState} name="fecha_Comprobante" value={proveedor&&direccion_proveedor}/>
+        <TextField type="date" className={styles.inputMaterial} label="Fecha Emision"  onChange={actualizarState} name="fecha_emision" value={comprobante&&fecha_emision}/>
         <br/>
-        <TextField className={styles.inputMaterial} label="Tipo Movimiento" onChange={actualizarState} name="tipo_Movimiento" value={proveedor&&localidad_proveedor} />
+        <Label>Tipo Movimiento</Label>
+        <Select name="tipo_movimiento" value={comprobante&&tipo_movimiento} onChange={actualizarState}>
+          <option value="">--Seleccione--</option>
+          <option value="Ingreso">Ingreso</option>
+          <option value="Egreso">Egreso</option>
+
+        </Select>
         <br/>
-        <TextField type="file" className={styles.inputMaterial} /*BORRE A PATIR DE LABEL O CRASHEABA, PORQUE NO ACEPTABA ARCHIVOS QUE NO SEAN FILES */ />
+        <input type="file"  name='input=file' id='selectArchivo'/>
+        <a href={comprobante&&link_archivo} target="_blank">Archivo</a>
         <br/>
-        <TextField  className={styles.inputMaterial} label="Monto Total" onChange={actualizarState} name="monto_Total" value={proveedor&&email_proveedor} />
+        <TextField type="number" className={styles.inputMaterial} label="Monto Total" onChange={actualizarState} name="total_comprobante" value={comprobante&&total_comprobante} />
         <br/><br/>
         <div align="right">
-          <Button onClick={()=>update2(id_proveedor)} color='primary'>Editar</Button>
+          <Button onClick={()=>update2(id_comprobante)} color='primary'>Editar</Button>
           <Button onClick={()=>abrirCerrarModalEditar()}>Cancelar</Button>
         </div>
       </div>
@@ -212,13 +379,15 @@ const Comprobantes = () => {
       setModalEditar(!modalEditar)
     }
 
-    const seleccionarProveedor = (proveedor,caso)=>{
-      proveedorAgregado(proveedor);
+    const seleccionarProveedor = (id_comprobante,caso)=>{
+      setComprobante(id_comprobante);
       (caso === "Editar")&&abrirCerrarModalEditar();
     }
 
     useEffect(()=>{
         funcion();
+        datos();
+        dato()
     },[])
    
 
@@ -238,7 +407,7 @@ const Comprobantes = () => {
                 {
                     icon:"delete",
                     tooltip:"Eliminar",
-                    onClick: (event,rowData)=>handleEliminar(rowData.id_proveedor)
+                    onClick: (event,rowData)=>handleEliminar(rowData.id_comprobante)
                 },
                 //COMO AGREGAR OTRO ICONO BOTON?
 
