62380802933b83ad45c98f515950f6eb7e3553b7
Cafux9510 <cafux9510@gmail.com>
2022-10-06 09:54:16 -0300

    agregando cambios guardados

---
+++ Vespucio/src/componentes/Materias.jsx
@@ -0,0 +1,492 @@
+import { useEffect, useState } from "react";
+import {supabase} from "../Backend/client";
+import MaterialTable from "@material-table/core";
+import { Dialog } from 'primereact/dialog';
+import { Button } from "@material-ui/core";
+import {Modal,TextField} from "@material-ui/core"
+import { InputText } from 'primereact/inputtext';
+import {makeStyles} from "@material-ui/core/styles"
+import { amber } from "@material-ui/core/colors";
+import swal from "sweetalert";
+import styled from '@emotion/styled'
+import { Link } from "react-router-dom";
+
+
+const Label = styled.label`
+    flex: 0 0 100px;
+    text-align:center;
+
+`;
+
+const Main = styled.div `
+  margin-top: 7%
+`;
+
+const Select = styled.select`
+    display:block;
+    width:100%;
+    padding: 1rem;
+    border: 1px solid #e1e1e1;
+    -webkit-appearance:none;
+`
+
+const Campo= styled.div`
+    display:flex;
+    margin-bottom: 1rem;
+    align-items:center;
+`;
+
+const useStyles = makeStyles((theme)=>({
+  modal:{
+    position:"absolute",
+    width:400,
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2,4,3),
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)" 
+  },
+  iconos:{
+    cursor: "pointer"
+  },
+  inputMaterial:{
+    width: "100%"
+  }
+
+}))
+
+
+
+const Materias = () => {
+    //Statest
+    const [data,setData]=useState([])
+    const[modal,insertarModal]=useState(false)
+    const [modalEditar, setModalEditar]= useState(false);
+    const[materias,setMaterias]=useState({
+        nombre_materia:'',
+        descripcion_materia:'',
+        id_grado:'',
+        id_nivel:'',
+    });
+
+
+
+    //Funciones que tienen datos desde una api
+    const funcion = async()=>{
+        try {
+           const result= await supabase.from('materias')
+           .select(`
+              *,
+              grados(
+                nombre_grado,
+                nivelesEducativos(
+                    nombre_nivel
+                )
+              )
+          `)
+           .eq("isHabilitada_materia",true)
+           setData(result.data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    const update = async(id_materia)=>{
+      try {
+        const result= await supabase.from("materias")
+        .update({isHabilitada_materia:false})
+        .eq("id_materia",id_materia)
+
+
+       
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    const{nombre_materia,descripcion_materia,id_grado,id_nivel}=materias;
+
+    const update2=async(id_materia)=>{
+      try {
+        const result= await supabase.from("materias")
+        .update({nombre_materia,descripcion_materia,id_grado})
+        .eq("id_materia",id_materia)
+        
+        abrirCerrarModalEditar();
+        window.location.reload()
+       } catch (error) {
+        console.log(error)
+      }
+    }
+
+    const submit = async()=>{
+      try {
+        const {error,result}= await supabase.from("materias").insert({
+            nombre_materia,
+            descripcion_materia,
+            id_grado
+        });
+
+        abrirCerrarModalInsertar();
+        setData({
+          ...data,
+          result
+        })
+        window.location.reload()
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    const handleEliminar=(id_materia)=>{
+      swal({
+        title: "Estas seguro de eliminar este registro?",
+        text: "Una vez eliminado, no podras recuperar el archivo de vuelta!",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+      })
+      .then((willDelete) => {
+        if (willDelete) {
+          swal("Registro eliminado con exito!", {
+            icon: "success",
+          });
+          update(id_materia);
+        }
+        setTimeout(() => {
+          window.location.reload()
+        }, 2000);
+      });
+    }
+
+    //Configuracion del {/*DEJO LOS PARAMETROS DEL VALUE, SINO ME CRASHEA */}
+    const columnas=[ 
+        {title:"N°", field:"id_materia"},
+        {title:"Nombre", field:"nombre_materia"},
+        {title:"Nivel", field:"grados.nivelesEducativos.nombre_nivel"},
+        {title:"Grado", field:"grados.nombre_grado"},
+        {title:"Descripcion", field:"descripcion_materia"}
+      ]
+
+
+    //Estilos
+    const styles=useStyles();
+    const actualizarState = e =>{
+        
+        setMaterias({
+          ...materias,
+          [e.target.name]: e.target.value
+      })
+    }
+  
+
+  //Funcion que me trae la lista de categorias y lo agrega a la lista desplegable
+  const[gradosMaterias,setGrados]=useState({}) 
+  const datos=async()=>{
+    const result = await supabase.from('grados')
+    .select()
+    .eq("isHabilitado_grado",true);
+
+    setGrados(result.data)
+    console.log(result.data)
+  }
+
+  const[niveles,setNiveles]=useState({}) 
+  const nivs=async()=>{
+    const result = await supabase.from('nivelesEducativos')
+    .select();
+
+    setNiveles(result.data)
+    return result.data
+  }
+
+  const filtrarAnios = e =>{
+
+    setMaterias({
+      ...materias,
+      [e.target.name]: e.target.value
+    })
+
+    var selection = document.getElementById("id_nivel");
+    var valor = selection.options[selection.selectedIndex].value
+
+    if (valor == 1) {
+      const datos=async()=>{
+        const result = await supabase.from('grados')
+        .select()
+        .eq("isHabilitado_grado",true)
+        .eq("nivelEduc",1);
+    
+        setGrados(result.data)
+      }
+      datos();
+
+    }else if(valor == 2){
+      const datos=async()=>{
+        const result = await supabase.from('grados')
+        .select()
+        .eq("isHabilitado_grado",true)
+        .eq("nivelEduc",2);
+    
+        setGrados(result.data)
+      }
+      datos();
+    }else{
+      const datos=async()=>{
+        const result = await supabase.from('grados')
+        .select()
+        .eq("isHabilitado_grado",true)
+        .eq("nivelEduc",3);
+    
+        setGrados(result.data)
+      }
+      datos();
+    }
+      
+    
+
+      
+  }
+
+
+    const bodyInsertar= (
+      <div className={styles.modal}>
+        <h3>Agregar Nuevo Insumo</h3> {/*DEJO LOS PARAMETROS DEL VALUE, SINO ME CRASHEA */}
+        <TextField className={styles.inputMaterial} label="Nombre Materia" onChange={actualizarState} name="nombre_materia" value={nombre_materia}/> 
+        <br/>
+        <br/>
+        <label>Indique el Nivel Educativo de la Materia</label>
+         <br/>
+         <br/>
+            <Campo>
+            <Select
+                        name='id_nivel'
+                        id='id_nivel'
+                        value={id_nivel}
+                        onChange={filtrarAnios}
+                    >
+                        <option value="">--Seleccione--</option>
+                        {Object.values(niveles).map(pr=>(
+                        <option key={pr.id_nivel} value={pr.id_nivel}>{pr.nombre_nivel}</option>
+                        ))}
+                    
+                        
+                    
+                </Select>
+            </Campo>
+         <br/>
+         <label>Indique el Grado de la Materia</label>
+         <br/>
+        <Campo>
+          <Select
+                    name='id_grado'
+                    value={id_grado}
+                    onChange={actualizarState}
+                >
+                    <option value="">--Seleccione--</option>
+                    {Object.values(gradosMaterias).map(pr=>(
+                      <option key={pr.id_grado} value={pr.id_grado}>{pr.nombre_grado}</option>
+                    ))}
+                
+                    
+                  
+            </Select>
+        </Campo>
+        <br/>
+        <label>Descripcion de la Materia</label>
+        <br/>
+        <br/>
+        <textarea type="text" className={styles.inputMaterial} label="Descripcion" onChange={actualizarState} name="descripcion_materia" value={descripcion_materia} />
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <div align="right">
+          <Button color='primary' onClick={()=>submit()} >Insertar</Button>
+          <Button onClick={()=>abrirCerrarModalInsertar()}>Cancelar</Button>
+        </div>
+      </div>
+    )
+
+    const{id_materia}=materias;
+
+    const bodyEditar= (
+        <div className={styles.modal}>
+        <h3>Agregar Nuevo Insumo</h3> {/*DEJO LOS PARAMETROS DEL VALUE, SINO ME CRASHEA */}
+        <TextField className={styles.inputMaterial} label="Nombre Materia" onChange={actualizarState} name="nombre_materia" value={materias&&nombre_materia}/> 
+        <br/>
+        <br/>
+        <label>Indique el Nivel Educativo de la Materia</label>
+         <br/>
+         <br/>
+            <Campo>
+            <Select
+                        name='id_nivel'
+                        id='id_nivel'
+                        value={id_nivel}
+                        onChange={filtrarAnios}
+                    >
+                        <option value="">--Seleccione--</option>
+                        {Object.values(niveles).map(pr=>(
+                        <option key={pr.id_nivel} value={pr.id_nivel}>{pr.materias&&nombre_nivel}</option>
+                        ))}
+                    
+                        
+                    
+                </Select>
+            </Campo>
+         <br/>
+         <label>Indique el Grado de la Materia</label>
+         <br/>
+        <Campo>
+          <Select
+                    name='id_grado'
+                    value={id_grado}
+                    onChange={actualizarState}
+                >
+                    <option value="">--Seleccione--</option>
+                    {Object.values(gradosMaterias).map(pr=>(
+                      <option key={pr.id_grado} value={pr.id_grado}>{pr.materias&&nombre_grado}</option>
+                    ))}
+                
+                    
+                  
+            </Select>
+        </Campo>
+        <br/>
+        <label>Descripcion de la Materia</label>
+        <br/>
+        <br/>
+        <textarea type="text" className={styles.inputMaterial} label="Descripcion" onChange={actualizarState} name="descripcion_materia" value={materias&&descripcion_materia} />
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <div align="right">
+          <Button color='primary' onClick={()=>submit()} >Insertar</Button>
+          <Button onClick={()=>abrirCerrarModalInsertar()}>Cancelar</Button>
+        </div>
+      </div>
+    )
+
+
+    /*ACA TAMBIEN MODIFICO FACUNDO-C1*/
+
+    const selectProduct=(event)=>{
+      
+      const product= nombres_prod.find(materia=>materia.id_materia == event.target.value);
+      console.log(product)
+      setMaterias(product)
+    }
+
+  
+    const sumar = e=>{
+        setMaterias({
+        ...materias,
+        [e.target.name]: e.target.value
+    })
+      
+    }
+
+  
+    //Funciones
+    const abrirCerrarModalInsertar= ()=>{
+      insertarModal(!modal)
+      setMaterias({})
+    }
+
+    /*agregado por FACUNDO-C1*/
+    const abrirCerrarModalActualizarStock= ()=>{
+      stockModal(!modalEditarStock)
+      setMaterias({})
+    }
+    /*HASTA ACA*/
+
+    const abrirCerrarModalEditar= ()=>{
+      setModalEditar(!modalEditar)
+    }
+
+    const seleccionarInsumo = (materias,caso)=>{
+        setMaterias(materias);
+      (caso === "Editar")&&abrirCerrarModalEditar();
+    }
+
+    useEffect(()=>{
+        funcion();
+        datos();
+        nivs();
+    },[])
+   
+
+  return (
+    <Main>
+       
+        <MaterialTable
+            title="Materias"
+            columns={columnas}
+            data={data}
+            actions={[
+                {
+                    icon:"edit",
+                    tooltip:"Modificar",
+                    onClick: (event,rowData)=>seleccionarInsumo(rowData,"Editar")
+                },
+                {
+                    icon:"delete",
+                    tooltip:"Eliminar",
+                    onClick: (event,rowData)=>handleEliminar(rowData.id_materia)
+                },
+                //COMO AGREGAR OTRO ICONO BOTON?
+
+                
+            ]}
+            
+
+            options={{
+                  actionsColumnIndex: -1,
+                  searchFieldStyle:{
+                    placeContent:"Buscar"
+                  }
+              }}
+            
+              localization={{
+                header:{
+                  actions:"Acciones",
+                  
+                },
+                toolbar:{
+                  searchPlaceholder:"Buscar"
+                }
+                
+              }}
+              
+        />
+
+        <Modal
+          open={modal}
+          onClose={abrirCerrarModalInsertar}
+        >
+          {bodyInsertar}
+        </Modal>
+            
+        <Modal
+          open={modalEditar}
+          onClose={abrirCerrarModalEditar}
+        >
+          {bodyEditar}
+        </Modal>
+
+
+        <div className="contenedor">
+          <br/>
+          <button className="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-slate-700 boton" onClick={()=>abrirCerrarModalInsertar()}>Registrar Nuevo Insumo</button>
+           
+
+          <br/><br/>
+        </div>
+    </Main>
+  )
+}
+
+export default Materias
---
+++ Vespucio/src/componentes/PersonalEducativo.jsx
@@ -0,0 +1,365 @@
+import { useEffect, useState,useRef } from "react";
+import {supabase} from "../Backend/client";
+import MaterialTable from "@material-table/core";
+import { Button } from "@material-ui/core";
+import {Modal,TextField} from "@material-ui/core"
+import {makeStyles} from "@material-ui/core/styles"
+import swal from "sweetalert";
+import styled from '@emotion/styled'
+import { Link } from "react-router-dom";
+import { Toast } from 'primereact/toast';
+
+const Label = styled.label`
+    flex: 0 0 100px;
+    text-align:center;
+
+`;
+const Main = styled.div `
+  margin-top: 7%
+`;
+const Select = styled.select`
+    display:block;
+    width:100%;
+    padding: 1rem;
+    border: 1px solid #e1e1e1;
+    -webkit-appearance:none;
+`
+
+const Campo= styled.div`
+    display:flex;
+    margin-bottom: 1rem;
+    align-items:center;
+`;
+
+
+const useStyles = makeStyles((theme)=>({
+  modal:{
+    position:"absolute",
+    width:400,
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2,4,3),
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)" 
+  },
+  iconos:{
+    cursor: "pointer"
+  },
+  inputMaterial:{
+    width: "100%"
+  }
+
+}))
+
+
+const PersonalEducativo = () => {
+    //Statest
+    const [data,setData]=useState([])
+    const[modal,insertarModal]=useState(false)
+    const [modalEditar, setModalEditar]= useState(false);
+    const[personal,personalAgregado]=useState({
+        nombre_personal:'',
+        telefono_personal:'',
+        mail_personal:'',
+        domicilio_personal:'',
+        apellido_personal:'',
+        dni_personal:'',
+    })
+    const toast = useRef(null);
+
+
+   
+    //Funciones que tienen datos desde una api
+    const funcion = async()=>{
+        try {
+           /* const result= await supabase.from('proveedores')
+           .select()
+           .eq("isHabilitado_proveedor",true)
+           setData(result.data) */
+           const { data: personal, error } = await supabase
+            .from('personalEducativo')
+            .select(`*`)
+            .eq("isHabilitado_personal",true)
+            .order("id_personal",{ascending:true})
+        
+            setData(personal)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    const update = async(id_personal)=>{
+      try {
+        const result= await supabase.from("personalEducativo")
+        .update({isHabilitado_personal:false})
+        .eq("id_personal",id_personal)
+
+        const arrayProveedores = data.filter(provee=>provee.id_personal !==id_personal)
+        setData(arrayProveedores)
+
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    const{nombre_personal,telefono_personal,mail_personal,domicilio_personal,apellido_personal,dni_personal}=personal;
+
+    const update2=async(id_personal)=>{
+      try {
+        const {result,error}= await supabase.from("personalEducativo")
+        .update({nombre_personal,telefono_personal,mail_personal,domicilio_personal,apellido_personal,dni_personal})
+        .eq("id_personal",id_personal)
+        funcion()
+
+
+        const arrayProveedores = data.map((prove)=>{
+          if(prove.id_personal === id_personal ){
+            return{
+              ...prove,
+              cuit_proveedor,
+              nombre_proveedor,
+              direccion_proveedor,
+              localidad_proveedor,
+              telefono_proveedor,
+              email_proveedor,
+              id_categoria_proveedor
+            }
+          }
+          return prove
+        })
+
+        setData(arrayProveedores)
+        abrirCerrarModalEditar();
+        toast.current.show({ severity: 'success', summary: 'Exito!', detail: 'Registro Modificado', life: 3000 });    
+       } catch (error) {
+        console.log(error)
+      }
+    }
+
+    const submit = async()=>{
+      try {
+        const result= await supabase.from("personalEducativo").insert({
+            nombre_personal,
+            telefono_personal,
+            mail_personal,
+            domicilio_personal,
+            apellido_personal,
+            dni_personal
+        });
+        funcion()
+
+        const resultado=result.data[0]
+        setData([...data,resultado])
+        abrirCerrarModalInsertar();
+       
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    const handleEliminar=(id_personal)=>{
+      swal({
+        title: "Estas seguro de eliminar este registro?",
+        text: "Una vez eliminado, no podras recuperar el archivo de vuelta!",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+      })
+      .then((willDelete) => {
+        if (willDelete) {
+          swal("Registro eliminado con exito!", {
+            icon: "success",
+          });
+          update(id_personal);
+        }
+     /*     setTimeout(() => {
+          window.location.reload()
+        }, 1000);  */
+      });
+    }
+
+    //Configuracion del 
+    const columnas=[
+        {title:"Identificador", field:"id_personal"},
+        {title:"Nombre", field:"nombre_personal"},
+        {title:"Apellido", field:"apellido_personal"},
+        {title:"DNI", field:"dni_personal"},
+        {title:"Domicilio", field:"domicilio_personal"},
+        {title:"Telefono",field:"telefono_personal"},
+        {title:"Email", field:"mail_personal"},
+      ]
+
+
+
+    //Estilos
+    const styles=useStyles();
+    const actualizarState = e =>{
+        personalAgregado({
+          ...personal,
+          [e.target.name]: e.target.value
+      })
+  }
+
+
+    const bodyInsertar= (
+        
+        <div className={styles.modal}>
+        <h4>Agregar Nuevo Personal Educativo</h4>
+        <br/>
+        <TextField className={styles.inputMaterial} label="Nombre"  onChange={actualizarState} name="nombre_personal" value={nombre_personal} />
+        <br/>
+        <br/>
+        <TextField className={styles.inputMaterial} label="Apellido" onChange={actualizarState} name="apellido_personal" value={apellido_personal} />
+        <br/>
+        <br/>
+        <TextField className={styles.inputMaterial} label="DNI" onChange={actualizarState} name="dni_personal" value={dni_personal} />
+        <br/>
+        <br/>
+        <TextField type="number" className={styles.inputMaterial} label="Telefono" onChange={actualizarState} name="telefono_personal" value={telefono_personal} />
+        <br/>
+        <br/>
+        <TextField type="email" className={styles.inputMaterial} label="Email" onChange={actualizarState} name="mail_personal" value={mail_personal} />
+        <br/>
+        <br/>
+        <TextField className={styles.inputMaterial} label="Domicilio" onChange={actualizarState} name="domicilio_personal" value={domicilio_personal}/>
+        <br/>
+        <br/><br/><br/>
+        <div align="right">
+          <Button color='primary' onClick={()=>submit()} >Insertar</Button>
+          <Button onClick={()=>abrirCerrarModalInsertar()}>Cancelar</Button>
+        </div>
+      </div>
+      
+    )
+
+
+    
+
+
+    const {id_personal}=personal;
+
+
+    const bodyEditar= (
+        <div className={styles.modal}>
+        <h4>Editar Personal Educativo</h4>        
+        <TextField className={styles.inputMaterial} label="Nombre" onChange={actualizarState} name="nombre_personal" value={personal&&nombre_personal} />
+        <br/>
+        <br/>
+        <TextField className={styles.inputMaterial} label="Apellido" onChange={actualizarState} name="apellido_personal" value={personal&&apellido_personal} />
+        <br/>
+        <br/>
+        <TextField className={styles.inputMaterial} label="DNI" onChange={actualizarState} name="dni_personal" value={personal&&dni_personal} />
+        <br/>
+        <br/>
+        <TextField type="number" className={styles.inputMaterial} label="Telefono" onChange={actualizarState} name="telefono_personal" value={personal&&telefono_personal} />
+        <br/>
+        <br/>
+        <TextField type="email" className={styles.inputMaterial} label="Email" onChange={actualizarState} name="mail_personal" value={personal&&mail_personal} />
+        <br/>
+        <br/>
+        <TextField className={styles.inputMaterial} label="Direccion" onChange={actualizarState} name="domicilio_personal" value={personal&&domicilio_personal}/>
+        <br/>
+        <br/><br/>
+        <div align="right">
+          <Button onClick={()=>update2(id_personal)} color='primary'>Editar</Button>
+          <Button onClick={()=>abrirCerrarModalEditar2()}>Cancelar</Button>
+        </div>
+      </div>
+    )
+
+
+    //Funciones
+    const abrirCerrarModalInsertar= ()=>{
+        personalAgregado({})
+      insertarModal(!modal)
+    }
+
+    const abrirCerrarModalEditar2= ()=>{
+      setModalEditar(!modalEditar)
+      personalAgregado({})
+    }
+    const abrirCerrarModalEditar= ()=>{
+      setModalEditar(!modalEditar)
+      
+    }
+
+    const seleccionarProveedor = (personal,caso)=>{
+        personalAgregado(personal);
+      (caso === "Editar")&&abrirCerrarModalEditar();
+    }
+
+    useEffect(()=>{
+        funcion();
+
+    },[])
+
+
+  return (
+    <Main>
+
+
+        <MaterialTable
+            title="Personal Educativo"
+            columns={columnas}
+            data={data}
+            actions={[
+                {
+                    icon:"edit",
+                    tooltip:"Modificar",
+                    onClick: (event,rowData)=>seleccionarProveedor(rowData,"Editar")
+                },
+                {
+                    icon:"delete",
+                    tooltip:"Eliminar",
+                    onClick: (event,rowData)=>handleEliminar(rowData.id_personal)
+                }
+                
+            ]}
+            
+
+            options={{
+                  actionsColumnIndex: -1,
+                  
+              }}
+            
+              localization={{
+                header:{
+                  actions:"Acciones",
+                  
+                },
+                toolbar:{
+                  searchPlaceholder:"Buscar"
+                }
+                
+              }}
+              
+        />
+        
+        <div className="contenedor">
+          <br/>
+          <button className="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-slate-700 boton" onClick={()=>abrirCerrarModalInsertar()}>Agregar Nuevo Personal Educativo</button>
+          <br/>
+          
+        </div>
+        
+        <Modal
+          open={modal}
+          onClose={abrirCerrarModalInsertar}
+        >
+          {bodyInsertar}
+        </Modal>
+            
+        <Modal
+          open={modalEditar}
+          onClose={abrirCerrarModalEditar}
+        >
+          {bodyEditar}
+        </Modal>
+    </Main>
+  )
+}
+
+
+ 
+export default PersonalEducativo
